dfbb126b118fed0766d8128c7735e86de9644b41
anthony <thonyarifin@gmail.com>
2020-08-17 01:53:54 +0800

    Added test for invoice receipt

--- Test/Form1.cs
+++ Test/Form1.cs
@@ -31,7 +31,44 @@ namespace Test
             var report = new InvoicePayment();
             report.Data = data;
             report.PopulateData();
-            report.PrintReceiptDialog();
+            report.ShowPreviewDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var data = new ReceiptData();
+
+            data.CompanyName = "PUDDING POP";
+            data.CompanyAddress = "NULL";
+            data.CompanyPhoneNo = "0778-423004";
+            
+            data.OrderNo = "123123123123";
+
+            data.OrderList = new List<ReceiptData.OrderDetails>
+            {
+                new ReceiptData.OrderDetails
+                {
+                    ItemName = "Sabun Cuci",
+                    Count = "5",
+                    UnitPrice = "40.000",
+                    Discount = "10",
+                    Total = "200.000"
+                },
+                new ReceiptData.OrderDetails
+                {
+                    ItemName = "If you need to do something different with the last element then you'd need something like",
+                    Count = "1",
+                    UnitPrice = "550.000",
+                    Discount = "0",
+                    Total = "550.000"
+                },
+            };
+
+            var report = new InvoiceReceipt();
+            report.Data = data;
+            report.Type = ReceiptType.Purchase;
+            report.PopulateData();
+            report.ShowPreviewDialog();
         }
     }
 }
