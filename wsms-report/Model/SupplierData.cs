40e738c5bec251fb96b564166223d782059ea669
anthony <thonyarifin@gmail.com>
2020-09-06 15:07:51 +0800

    Added supplier report

---
+++ wsms-report/Model/SupplierData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wsms.report.Model
+{
+    public class SupplierData
+    {
+        public string CompanyName { get; set; }
+        public string ReportTitle { get; set; }
+        public List<Supplier> SupplierList { get; set; }
+
+        public class Supplier
+        {
+            public string Name { get; set; }
+            public string PhoneNo { get; set; }
+            public string Address { get; set; }
+            public string Debt { get; set; }
+        }
+    }
+}
