0241ec1073b1eabeed94dde55f3406c786f21ec4
anthony <thonyarifin@gmail.com>
2020-09-06 15:03:46 +0800

    Added customer report

---
+++ wsms-report/CustomerReport.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Drawing.Printing;
+using System.Linq;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.UI;
+using wsms.report.Model;
+
+namespace wsms.report
+{
+    public partial class CustomerReport : DevExpress.XtraReports.UI.XtraReport
+    {
+        public CustomerData Data { get; set; }
+
+        public delegate void PrintComplete(object o, PrintEventArgs e);
+        public event PrintComplete OnPrintingReceiptComplete;
+
+        public CustomerReport()
+        {
+            InitializeComponent();
+        }
+
+        public void PopulateData()
+        {
+            if (Data != null)
+            {
+                lblCompanyName.Text = Data.CompanyName;
+                lblTitle.Text       = Data.ReportTitle;
+                lblPrintDate.Text   = DateTime.Now.ToString("dd/MM/yyyy");
+                lblTotalItem.Text   = Data.CustomerList.Count.ToString();
+
+                if (Data.CustomerList != null && Data.CustomerList.Count > 0)
+                {
+                    var i = 1;
+                    var templateRow = tblDetails.Rows[1];
+                    var currRow = templateRow;
+
+                    foreach (var item in Data.CustomerList)
+                    {
+                        currRow.Cells[0].Text = i.ToString();
+                        currRow.Cells[1].Text = item.Name;
+                        currRow.Cells[2].Text = item.Level;
+                        currRow.Cells[3].Text = item.PhoneNo;
+                        currRow.Cells[4].Text = item.Address;
+                        currRow.Cells[5].Text = item.Debt;
+
+                        if (!Data.CustomerList.Last().Equals(item))
+                        {
+                            tblDetails.InsertRowBelow(currRow);
+                            i++;
+                            currRow = tblDetails.Rows[i];
+
+                            currRow.Cells[0].TextAlignment = TextAlignment.MiddleCenter;
+
+                            currRow.Cells[1].TextAlignment = TextAlignment.MiddleLeft;
+                            currRow.Cells[1].Padding = templateRow.Cells[1].Padding;
+
+                            currRow.Cells[2].TextAlignment = TextAlignment.MiddleLeft;
+                            currRow.Cells[2].Padding = templateRow.Cells[1].Padding;
+
+                            currRow.Cells[3].TextAlignment = TextAlignment.MiddleLeft;
+                            currRow.Cells[3].Padding = templateRow.Cells[1].Padding;
+
+                            currRow.Cells[4].TextAlignment = TextAlignment.MiddleLeft;
+                            currRow.Cells[4].Padding = templateRow.Cells[1].Padding;
+
+                            currRow.Cells[5].TextAlignment = TextAlignment.MiddleRight;
+                            currRow.Cells[5].Padding = templateRow.Cells[1].Padding;
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool ValidateForm()
+        {
+            return !string.IsNullOrEmpty(lblCompanyName.Text) &&
+                !string.IsNullOrEmpty(lblTotalItem.Text);
+        }
+
+        public void PrintReceiptDialog()
+        {
+            if (ValidateForm())
+            {
+                using (ReportPrintTool printTool = new ReportPrintTool(this))
+                {
+                    // Invoke the Print dialog.
+                    printTool.PrintDialog();
+                    printTool.PrintingSystem.StartPrint += new DevExpress.XtraPrinting.PrintDocumentEventHandler(PrintingSystem_StartPrint);
+                }
+            }
+            else
+            {
+                throw new NullReferenceException("Report data hasn't populated");
+            }
+        }
+
+        public void PrintReceipt()
+        {
+            if (ValidateForm())
+            {
+                using (ReportPrintTool printTool = new ReportPrintTool(this))
+                {
+                    // Invoke the Print dialog.
+                    printTool.Print();
+                    printTool.PrintingSystem.StartPrint += new DevExpress.XtraPrinting.PrintDocumentEventHandler(PrintingSystem_StartPrint);
+                }
+            }
+            else
+            {
+                throw new NullReferenceException("Report data hasn't populated");
+            }
+        }
+
+
+        void PrintingSystem_StartPrint(object sender, DevExpress.XtraPrinting.PrintDocumentEventArgs e)
+        {
+            e.PrintDocument.EndPrint += new PrintEventHandler(PrintDocument_EndPrint);
+        }
+
+        void PrintDocument_EndPrint(object sender, PrintEventArgs e)
+        {
+            if (OnPrintingReceiptComplete != null)
+                OnPrintingReceiptComplete(sender, e);
+        }
+
+        public void ShowPrintPreview()
+        {
+            if (ValidateForm())
+            {
+                using (ReportPrintTool printTool = new ReportPrintTool(this))
+                {
+                    var ps = printTool.PrintingSystem;
+                    ps.SetCommandVisibility(PrintingSystemCommand.Background, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.Watermark, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.Save, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.FillBackground, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.Open, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.ClosePreview, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.Find, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.ExportMht, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.ExportRtf, CommandVisibility.None);
+
+                    ps.SetCommandVisibility(PrintingSystemCommand.SendCsv, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.SendFile, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.SendGraphic, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.SendMht, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.SendPdf, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.SendRtf, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.SendTxt, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.SendXls, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.SendXlsx, CommandVisibility.None);
+                    ps.SetCommandVisibility(PrintingSystemCommand.SendXps, CommandVisibility.None);
+
+                    printTool.ShowPreviewDialog();
+                }
+            }
+        }
+
+    }
+}
---
+++ wsms-report/Model/CustomerData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wsms.report.Model
+{
+    public class CustomerData
+    {
+        public string CompanyName { get; set; }
+        public string ReportTitle { get; set; }
+        public List<Customer> CustomerList { get; set; }
+
+        public class Customer
+        {
+            public string Name { get; set; }
+            public string Level { get; set; }
+            public string PhoneNo { get; set; }
+            public string Address { get; set; }
+            public string Debt { get; set; }
+        }
+    }
+}
